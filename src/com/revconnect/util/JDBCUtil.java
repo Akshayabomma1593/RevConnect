788343f9b12e257db22e3d4dda189bd77f0904e8
Mogarampalli Hema <mogarampallihema@gmail.com>
2026-01-29 11:36:00 +0530

    Update JDBCUtil.java

--- src/com/revconnect/util/JDBCUtil.java
+++ src/com/revconnect/util/JDBCUtil.java
@@ -6,16 +6,18 @@ import java.sql.DriverManager;
 public class JDBCUtil {
 
     private static final String URL =
-            "jdbc:oracle:thin:@//127.0.0.1:1521/ORCLPDB";
+            "jdbc:oracle:thin:@localhost:1521/XEPDB1";
     private static final String USERNAME = "revconnect";
     private static final String PASSWORD = "revconnect123";
 
     public static Connection getConnection() {
+
+        Connection con = null;
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");
-            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            con = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (Exception e) {
-            throw new RuntimeException("❌ Database connection failed", e);
+            e.printStackTrace();
         }
+        return con;
     }
-}
