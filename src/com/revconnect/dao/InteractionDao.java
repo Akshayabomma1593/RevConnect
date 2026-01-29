b829f6d4b049c6f41295bb234806458ff89f208f
Mogarampalli Hema <mogarampallihema@gmail.com>
2026-01-29 23:54:51 +0530

    Update InteractionDao.java

--- src/com/revconnect/dao/InteractionDao.java
+++ src/com/revconnect/dao/InteractionDao.java
@@ -1,11 +1,11 @@
 package com.revconnect.dao;
 
 import com.revconnect.model.Interaction;
-import com.revconnect.model.PostAnalytics;
+import java.util.List;
 
 public interface InteractionDao {
 
-    void saveInteraction(Interaction interaction);
-
-    PostAnalytics getPostAnalytics(int postId);
+    void likePost(int postId, int userId);
+    void commentOnPost(Interaction interaction);
+    List<Interaction> getCommentsByPost(int postId);
 }
