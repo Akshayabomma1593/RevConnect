85c20463e54dba332fe4a957402e86dfea6d1a8d
Saipavan6635 <saipavansiddam21@gmail.com>
2026-01-30 09:45:25 +0530

    Update ConnectionServiceImpl.java

--- src/com/revconnect/service/ConnectionServiceImpl.java
+++ src/com/revconnect/service/ConnectionServiceImpl.java
@@ -1,19 +1,44 @@
 package com.revconnect.service;
 
-import com.revconnect.dao.ConnectionDaoImpl;
-import com.revconnect.dao.IConnectionDao;
+import com.revconnect.dao.*;
+import com.revconnect.model.Connection;
+
+import java.util.List;
 
 public class ConnectionServiceImpl implements IConnectionService {
 
     private IConnectionDao connectionDao = new ConnectionDaoImpl();
+    private INotificationDao notificationDao = new NotificationDaoImpl();
 
     @Override
-    public boolean sendRequest(int senderId, int receiverId) {
-        return connectionDao.sendRequest(senderId, receiverId);
+    public void sendRequest(int requesterId, int targetUserId) {
+        connectionDao.sendRequest(requesterId, targetUserId);
+        notificationDao.createNotification(
+                targetUserId,
+                "CONNECTION_REQUEST",
+                requesterId,
+                "You received a connection request"
+        );
     }
 
     @Override
-    public boolean acceptRequest(int senderId, int receiverId) {
-        return connectionDao.acceptRequest(senderId, receiverId);
+    public void acceptRequest(int connectionId, int requesterId) {
+        connectionDao.updateStatus(connectionId, "ACCEPTED");
+        notificationDao.createNotification(
+                requesterId,
+                "CONNECTION_ACCEPTED",
+                connectionId,
+                "Your connection request was accepted"
+        );
+    }
+
+    @Override
+    public List<Connection> viewPending(int userId) {
+        return connectionDao.getPendingRequests(userId);
+    }
+
+    @Override
+    public List<Connection> viewConnections(int userId) {
+        return connectionDao.getConnections(userId);
     }
 }
