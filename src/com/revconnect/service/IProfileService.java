c415181715b7075c4a625285ad336fdc57013340
Akshaya Bomma <142657082+Akshayabomma1593@users.noreply.github.com>
2026-01-30 09:39:42 +0530

    Create IProfileService.java

---
+++ src/com/revconnect/service/IProfileService.java
@@ -0,0 +1,8 @@
+package com.revconnect.service;
+
+import com.revconnect.model.User;
+
+public interface IUserService {
+    void register(User user);
+    User login(String username, String password);
+}
