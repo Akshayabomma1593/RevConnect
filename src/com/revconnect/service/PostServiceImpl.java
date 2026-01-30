811bf4d8980fc841a048dbdf70d6bcd7758b6aca
Ambati Gowtham Reddy <119399374+AMBATI223@users.noreply.github.com>
2026-01-30 09:37:16 +0530

    Refactor PostServiceImpl for simplified post creation

--- src/com/revconnect/service/PostServiceImpl.java
+++ src/com/revconnect/service/PostServiceImpl.java
@@ -1,32 +1,24 @@
 package com.revconnect.service;
 
-import com.revconnect.dao.IPostDao;
-import com.revconnect.dao.PostDaoImpl;
+import com.revconnect.dao.*;
 import com.revconnect.model.Post;
 
 import java.util.List;
 
 public class PostServiceImpl implements IPostService {
 
-    private final IPostDao postDao = new PostDaoImpl();
+    private IPostDao postDao = new PostDaoImpl();
 
     @Override
-    public void createPostWithHashtags(Post post, List<String> hashtags) {
-
+    public void createPost(Post post) {
         int postId = postDao.createPost(post);
-
-        if (postId == -1) {
-            System.out.println("Post creation failed");
-            return;
+        if (postId > 0 && post.getHashtags() != null) {
+            postDao.saveHashtags(postId, post.getHashtags());
         }
+    }
 
-        for (String tag : hashtags) {
-            int hashtagId = postDao.createOrGetHashtag(tag);
-            if (hashtagId != -1) {
-                postDao.mapPostHashtag(postId, hashtagId);
-            }
-        }
-
-        System.out.println("Post created successfully with hashtags!");
+    @Override
+    public List<Post> viewAllPosts() {
+        return postDao.getAllPosts();
     }
 }
