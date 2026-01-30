2098cf4b2b0caf0aed53b347c53ee455710d7214
sriramvishwa <84197616+sriramvishwa@users.noreply.github.com>
2026-01-30 09:37:54 +0530

    Refactor MessageServiceImpl methods and imports

--- src/com/revconnect/service/MessageServiceImpl.java
+++ src/com/revconnect/service/MessageServiceImpl.java
@@ -1,27 +1,21 @@
 package com.revconnect.service;
 
-import com.revconnect.dao.IMessageDao;
-import com.revconnect.dao.MessageDaoImpl;
+import com.revconnect.dao.*;
 import com.revconnect.model.Message;
 
 import java.util.List;
 
 public class MessageServiceImpl implements IMessageService {
 
-    private final IMessageDao messageDao = new MessageDaoImpl();
+    private IMessageDao messageDao = new MessageDaoImpl();
 
     @Override
-    public boolean sendMessage(Message message) {
-        return messageDao.sendMessage(message);
+    public void sendMessage(Message message) {
+        messageDao.sendMessage(message);
     }
 
     @Override
-    public List<Message> viewInbox(int receiverId) {
-        return messageDao.getMessagesByReceiver(receiverId);
-    }
-
-    @Override
-    public boolean markMessageAsRead(int messageId) {
-        return messageDao.markAsRead(messageId);
+    public List<Message> viewChat(int user1, int user2) {
+        return messageDao.getChat(user1, user2);
     }
 }
