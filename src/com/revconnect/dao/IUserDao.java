facd8aec9f9c6e15c1de9b6bafe5a187378ab8d2
Akshaya Bomma <142657082+Akshayabomma1593@users.noreply.github.com>
2026-01-30 09:22:29 +0530

    Update IUserDao.java

--- src/com/revconnect/dao/IUserDao.java
+++ src/com/revconnect/dao/IUserDao.java
@@ -1,13 +1,10 @@
 package com.revconnect.dao;
 
-import com.revconnect.model.Profile;
 import com.revconnect.model.User;
 
 public interface IUserDao {
+    void register(User user);
+    User login(String username, String password);
 
-    boolean registerUser(User user);
 
-    User login(String email, String password);
-
-    Profile getProfileByUserId(int userId);
 }
