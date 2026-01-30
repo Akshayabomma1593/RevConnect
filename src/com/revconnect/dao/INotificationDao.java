49bae030062d198faf4c19cd32c82a061a5064da
Saipavan6635 <saipavansiddam21@gmail.com>
2026-01-30 09:40:19 +0530

    Update INotificationDao.java

--- src/com/revconnect/dao/INotificationDao.java
+++ src/com/revconnect/dao/INotificationDao.java
@@ -1,4 +1,18 @@
 package com.revconnect.dao;
+
+import com.revconnect.model.Notification;
+import java.util.List;
+
 public interface INotificationDao {
-    boolean createNotification(int userId, String message);
+
+    void createNotification(
+            int userId,
+            String type,
+            int referenceId,
+            String message
+    );
+
+    List<Notification> getNotifications(int userId);
+
+    void markAsRead(int notificationId);
 }
