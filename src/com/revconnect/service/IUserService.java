346b7474bfee6012e67e5d2bf4d4fa2046032f13
Akshaya Bomma <142657082+Akshayabomma1593@users.noreply.github.com>
2026-01-30 09:36:53 +0530

    Update IUserService.java

--- src/com/revconnect/service/IUserService.java
+++ src/com/revconnect/service/IUserService.java
@@ -1,13 +1,9 @@
 package com.revconnect.service;
 
-import com.revconnect.model.Profile;
 import com.revconnect.model.User;
 
 public interface IUserService {
-
-    boolean registerUser(User user);
-
-    User login(String email, String password);
-
-    Profile viewProfile(int userId);
+    void register(User user);
+    User login(String username, String password);
 }
+
